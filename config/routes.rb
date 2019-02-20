17f5e69bfb4b9704af245945c8572109b0f40a64
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2019-02-20 17:26:03 +0000

    routes started

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "restaurants"
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id', to: 'task#show', as: :task
+  get 'restaurants/:id/reviews/new', to:
+  post 'restaurants/:id/reviews'
 end
